d1aa89abe469d1e71163ec9b2336bd68ccb7e92e
railleous <harsh.ranger1999@gmail.com>
2023-12-14 14:38:33 +0530

    GET ELEMENT BY CLASS TUT

--- dom.js
+++ dom.js
@@ -15,10 +15,27 @@
 
 //console.log(document.getElementById('header-title'));
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent = 'Text';
-header.style.borderBottom = 'solid 3px #000';
-var listItems = document.getElementsByClassName('list-group-item');
-console.log(listItems[4].style.color = 'green');
+//header.style.borderBottom = 'solid 3px #000';
+//var listItems = document.getElementsByClassName('list-group-item');
+//console.log(listItems[4].style.color = 'green');
+
+//GETELEMENTSBYCLASSNAME//
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items)
+//items[2].textContent = 'Hello 2';
+items[2].style.backgroundColor = 'green';
+
+//for(var i=0 ; i < items.length; i++){
+//    items[i].style.backgroundColor = '#f4f4f4';
+//}
+
+for(var i=0 ; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+    items[i].style.color = 'purple';
+}
+
